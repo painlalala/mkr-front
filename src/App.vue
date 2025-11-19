488afd897f0751ffbad9c8717f826b0a33302815
Lozynskyi Nazarii <nazarii.lozynskyi@lantec.ua>
2025-11-19 10:59:08 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { RouterView } from 'vue-router'
+import HeaderNav from './components/HeaderNav.vue'
+import { isAuthenticated } from '@/stores/auth'
+</script>
+
+<template>
+  <HeaderNav v-if="isAuthenticated" />
+  <main class="app-main" :class="{ authed: isAuthenticated }">
+    <RouterView />
+  </main>
+</template>
+
+<style scoped>
+:host { display:block; }
+:root { --header-h: 63px }
+.app-main {
+  width: 100%;
+  min-height: 100vh;
+}
+.app-main.authed {
+  padding-top: var(--header-h);
+}
+</style>
